df7902accb8b647b09ed5bc85eb3c987f3874a21
Jordan Bjerken <jbjerken@gmail.com>
2018-03-07 15:43:34 -0500

    began test practice

---
+++ prac_quiz/practice/test1/main.cpp
@@ -0,0 +1,19 @@
+#include<ostream>
+
+class Pokemon{
+public:
+  Pokemon() : name(),
+    index(0),
+    ad(0),
+    defense(0){}
+  Pokemon(string n, int i, )
+private:
+  string name;
+  int index;
+  int ad;
+  int defense;
+}
+
+int main() {
+  return 0;
+}
